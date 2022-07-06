9e669b068e0eb8dd359f26a52b151e1159f1974f
AvneetLohiya <108623413+AvneetLohiya@users.noreply.github.com>
2022-07-06 21:48:08 +0530

    javaScript

---
+++ Index.js
@@ -0,0 +1,17 @@
+// 1.ways to print in javascript
+// console.log("Hello World");
+// alert("me");
+// document.write("this is document write");
+
+// 2. Javascript console API
+// console.log("Hello World", 4 + 4, "Another log");
+// console.warn("this is warning");
+// console.error("this is an error");
+// What are variables? - Containers to store data values
+
+/*multi 
+line
+comment*/
+var number1= 34;
+var number2= 56;
+console.log(number1 + number2);
